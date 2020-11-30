c43d6c351a493c50e1151213ff4b0c8b62121c02
Robert Stefanic <rstefanic72@gmail.com>
2020-11-30 11:03:07 -0500

    Added Vertical and Horizontal Datum

---
+++ src/constants.js
@@ -0,0 +1,35 @@
+export const HORIZONTAL_DATUM = [
+    'sg1897',
+    'sg1952',
+    'nad83(1986)',
+    'nad27',
+    'nad83(1992)',
+    'nad83(nsrs2007)',
+    'nad83(2011)',
+    'ussd',
+    'nad27',
+    'nad83(harn)',
+    'nad83(fbn)',
+    'nad83(nsrs2007)',
+    'ohd',
+    'nad83(1993)',
+    'nad83(pa11)',
+    'pr40',
+    'nad83(1997)',
+    'nad83(2002)',
+    'as62',
+    'gu63',
+    'nad83(ma11)'
+];
+
+export const VERTICAL_DATUM = [
+    'ngvd29',
+    'navd88',
+    'guvd63',
+    'guvd04',
+    'lt',
+    'nmvd03',
+    'vivd09',
+    'prvd02',
+    'asvd02'
+];
