b597777768b027d0ce2a8f2813ebbf9c7810ec14
Robert Stefanic <rstefanic72@gmail.com>
2020-11-30 11:07:38 -0500

    Expanded ncat test to validate queryParameters

--- tests/ncat.test.js
+++ tests/ncat.test.js
@@ -1,7 +1,29 @@
 import ncat from '../src/ncat';
+import { MissingRequiredQueryParameter, InvalidQueryParameter } from '../src/errors';
 
-describe('Test', () => {
-  it('add two numbers together', () => {
-    expect(ncat.add(1, 2)).toBe(3);
-  });
+describe('Latitude-longitude-height service', () => {
+    const VALID_REQUEST = {
+        lat: '40.0',
+        lon: '-80.0',
+        inDatum: 'nad83(1986)',
+        outDatum: 'nad83(2011)'
+    };
+
+    it('cannot make a request without the required fields', () => {
+        Object.keys(VALID_REQUEST)
+            .forEach(parameter => {
+                let currentRequest = { ...VALID_REQUEST };
+                delete currentRequest[parameter];
+
+                expect(() => ncat.llhServiceRequest(currentRequest))
+                    .toThrowError(MissingRequiredQueryParameter);
+            });
+    });
+
+    it('can validate raise exceptions for non-required fields', () => {
+        let currentRequest = { ...VALID_REQUEST };
+        currentRequest['eht'] = 'not a valid float';
+        expect(() => ncat.llhServiceRequest(currentRequest))
+            .toThrowError(InvalidQueryParameter);
+    });
 });
