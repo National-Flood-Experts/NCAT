33134f8cf6500c1e0957a5b57ea3c276556c76f5
Robert Stefanic <rstefanic72@gmail.com>
2020-11-30 13:32:56 -0500

    Removed NCAT class and exported the function directly

--- src/ncat.js
+++ src/ncat.js
@@ -1,17 +1,39 @@
 import axios from 'axios';
 import LLHSchema from './schemas/LLH';
 import validate from './validate';
+import utils from './utils';
 
-class NCAT {
-    static axios = axios.create({
+let NCATAxiosInstance = null;
+
+function getNCATAxiosInstance() {
+    if (NCATAxiosInstance) {
+        return NCATAxiosInstance;
+    }
+
+    NCATAxiosInstance = axios.create({
         baseURL: "https://geodesy.noaa.gov/api/ncat/",
         responseType: 'json',
         withCredentials: false
     });
 
-    static llhServiceRequest(queryParameters) {
-        validate(queryParameters, LLHSchema);
-    }
+    return NCATAxiosInstance;
 }
 
-export default NCAT;
+function LLHServiceRequest(queryParameters) {
+    validate(queryParameters, LLHSchema);
+    let queryString = utils.buildQueryString(queryParameters);
+
+    return getNCATAxiosInstance()
+        .get('llh' + queryString)
+        .then(response => {
+            let data = response.data;
+
+            if ('error' in data) {
+                return Promise.reject(data.error);
+            }
+
+            return data;
+        });
+}
+
+export default { LLHServiceRequest }
