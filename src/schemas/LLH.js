146deb7a76e7d07efaa07643abf408eaa9ceae1a
Robert Stefanic <rstefanic72@gmail.com>
2020-11-30 11:02:32 -0500

    Added schema for LLH service

---
+++ src/schemas/LLH.js
@@ -0,0 +1,52 @@
+import { HORIZONTAL_DATUM, VERTICAL_DATUM } from '../constants.js';
+
+export default {
+    lat: {
+        required: true,
+        validator: value => parseFloat(value) == Number(value),
+    },
+    lon: {
+        required: true,
+        validator: value => parseFloat(value) == Number(value),
+    },
+    eht: {
+        required: false,
+        validator: value => parseFloat(value) == Number(value),
+    },
+    inDatum: {
+        required: true,
+        validator: value => HORIZONTAL_DATUM.includes(value.toLowerCase())
+    },
+    outDatum: {
+        required: true,
+        validator: value => HORIZONTAL_DATUM.includes(value.toLowerCase())
+    },
+    spcZone: {
+        required: false,
+        validator: value => parseInt(value) == Number(value) && value.length === 4
+    },
+    utmZone: {
+        required: false,
+        validator: value => parseInt(value) == Number(value) && value.length === 4
+    },
+    a: {
+        required: false,
+        validator: value => parseFloat(value) == Number(value)
+    },
+    invf: {
+        required: false,
+        validator: value => parseFloat(value) == Number(value)
+    },
+    inVertDatum: {
+        required: false,
+        validator: value => VERTICAL_DATUM.includes(value.toLowerCase())
+    },
+    outVerDatum: {
+        required: false,
+        validator: value => VERTICAL_DATUM.includes(value.toLowerCase())
+    },
+    orthoHt: {
+        required: false,
+        validator: value => parseFloat(value) == Number(value),
+    }
+}
